f06789529a04c5cff0badc461732352fc78b6029
cbaredline <119635127+cbaredline@users.noreply.github.com>
2024-12-12 09:49:38 +0100

    music

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -9,8 +9,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+app.get('/:page', (req, res) => {
+    const page = req.params.page;
+    const filePath = path.join(__dirname, 'public', `${page}.html`);
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            res.status(404).send('Page Not Found'); // Handle missing files
+        }
+    });
+});
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:3000');
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
